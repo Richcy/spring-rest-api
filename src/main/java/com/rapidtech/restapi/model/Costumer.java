8a88f8d846234d4e2733f790564ddd6cb008568f
Richcy Dian Sukma <richcyveliz@gmail.com>
2022-12-20 17:22:21 +0700

    Checkpoint Study: Teams Meet End Today

---
+++ src/main/java/com/rapidtech/restapi/model/Costumer.java
@@ -0,0 +1,19 @@
+package com.rapidtech.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+
+public class Costumer {
+    private int id;
+    private String fullName;
+    private String address;
+    private String city;
+    private String gender;
+    private Date dateOfBirth;
+    private String dateOfPlace;
+
+}
---
+++ src/test/java/com/rapidtech/restapi/controller/ProductControllerTest.java
@@ -0,0 +1,7 @@
+package com.rapidtech.restapi.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+}
